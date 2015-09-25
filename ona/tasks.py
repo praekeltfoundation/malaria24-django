d35924a2b73232be9a5fce6c5bfa616d0b2c9751
Simon de Haan <simon@praekeltfoundation.org>
2015-09-25 07:52:16 +0200

    install onapie

--- ona/tasks.py
+++ ona/tasks.py
@@ -1,6 +1,10 @@
+from django.conf import settings
+
 from malaria24 import celery_app
+
+from onapie.client import Client
 
 
 @celery_app.task
-def test_task():
-    return 'hi'
+def ona_fetch_reported_cases():
+    client = Client('https://ona.io', api_token=settings.ONAPI_ACCESS_TOKEN)
