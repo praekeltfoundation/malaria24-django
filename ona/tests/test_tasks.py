a2139f0a6d274bafda90ec5c2026ae88da4f511e
Simon de Haan <simon@praekeltfoundation.org>
2015-09-25 08:52:43 +0200

    getting tests to run

---
+++ ona/tests/test_tasks.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+import pytest
+
+
+@pytest.mark.django_db
+class OnaTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_ona_fetch_reported_cases_task(self):
+        pass
