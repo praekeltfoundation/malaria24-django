fae560cbbd3f4f35263c48da41b804381bf584dd
Simon de Haan <simon@praekeltfoundation.org>
2015-09-28 16:41:07 +0200

    mocking ona requests

--- ona/tests/test_tasks.py
+++ ona/tests/test_tasks.py
@@ -1,13 +1,19 @@
 from django.test import TestCase
 
+import pkg_resources
 import pytest
+import responses
 
 
 @pytest.mark.django_db
+@responses.activate
 class OnaTest(TestCase):
 
     def setUp(self):
-        pass
+        responses.add(responses.GET, 'https://ona.io/api/v1/',
+                      status=200, content_type='application/json',
+                      body=pkg_resources.resource_string(
+                          'malaria24', 'ona/fixtures/responses/catalog.json'))
 
     def tearDown(self):
         pass
