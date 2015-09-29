8abdfffe11c262c9d2f3a99c6ee0a7938bb092d7
Simon de Haan <simon@praekeltfoundation.org>
2015-09-29 13:04:55 +0200

    model admin for reported cases

--- malaria24/ona/admin.py
+++ malaria24/ona/admin.py
@@ -3,6 +3,21 @@ from django.contrib import admin
 from .models import ReportedCase, EHP, SMS
 
 
-admin.site.register(ReportedCase)
+class ReportedCaseAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = ('first_name',
+                    'last_name',
+                    'locality',
+                    'sa_id_number',
+                    'msisdn',
+                    'id_type',
+                    'gender',
+                    'facility_code',
+                    'landmark',
+                    'created_at')
+    list_filter = ('facility_code', 'gender', 'created_at')
+
+
+admin.site.register(ReportedCase, ReportedCaseAdmin)
 admin.site.register(EHP)
 admin.site.register(SMS)
