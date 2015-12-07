b4c11614e20aba9c2c6c2aa81ed9c5242ef95994
codiebeulaine <codiebeulaine@gmail.com>
2015-12-07 18:46:03 +0200

    national and provincial reports complete with tests

--- malaria24/ona/tests/test_models.py
+++ malaria24/ona/tests/test_models.py
@@ -432,12 +432,18 @@ class DigestTest(MalariaTestCase):
         self.mk_mis(name='MIS', email_address='mis@example.org')
 
         for i in range(10):
-            case = self.mk_case()
+            case = self.mk_case(gender='female', facility_code='342315')
             case.date_of_birth = datetime.today().strftime("%y%m%d")
             case.ehps.add(ehp1)
             case.ehps.add(ehp2)
-            case.gender = 'female'
-            case.facility_code = '342315'
+            case.save()
+            case.digest = None
+
+        for i in range(10):
+            case = self.mk_case(gender='male', facility_code='222222')
+            case.date_of_birth = datetime.today().strftime("%y%m%d")
+            case.ehps.add(ehp1)
+            case.ehps.add(ehp2)
             case.save()
             case.digest = None
 
@@ -449,9 +455,11 @@ class DigestTest(MalariaTestCase):
         data = digest.get_digest_email_data()
         self.assertEqual(data['provinces'][4]['females'], 10)
         self.assertEqual(data['provinces'][4]['males'], 0)
+        self.assertEqual(data['provinces'][0]['males'], 10)
         self.assertEqual(data['provinces'][4]['province'], 'Limpopo')
         self.assertEqual(data['provinces'][4]['under5'], 10)
         self.assertEqual(data['provinces'][4]['over5'], 0)
+        self.assertEqual(len(data['provinces']), 9)
         self.assertEqual(
             message.to, ['manager@example.org', 'mis@example.org'])
 
@@ -463,24 +471,42 @@ class DigestTest(MalariaTestCase):
                                 district=u'Example1')
         Facility.objects.create(facility_code='222222',
                                 facility_name='Facility 2',
+                                province='Limpopo',
+                                district=u'Example2')
+        Facility.objects.create(facility_code='333333',
+                                facility_name='Facility 2',
                                 province='The Eastern Cape',
                                 district=u'Example2')
         manager1 = self.mk_actor(role=MANAGER_PROVINCIAL,
                                  email_address='manager@example.org',
                                  facility_code='342315')
-        manager2 = self.mk_actor(role=MIS,
-                                 email_address='mis@example.org',
-                                 facility_code='342315')
+        self.mk_actor(role=MIS,
+                      email_address='mis@example.org',
+                      facility_code='342315')
         ehp1 = self.mk_ehp(name='EHP1', email_address='ehp1@example.org')
         ehp2 = self.mk_ehp(name='EHP2', email_address='ehp2@example.org')
 
         for i in range(10):
-            case = self.mk_case()
+            case = self.mk_case(gender='female', facility_code='342315')
             case.date_of_birth = datetime.today().strftime("%y%m%d")
             case.ehps.add(ehp1)
             case.ehps.add(ehp2)
-            case.gender = 'female'
-            case.facility_code = '342315'
+            case.save()
+            case.digest = None
+
+        for i in range(10):
+            case = self.mk_case(gender='male', facility_code='222222')
+            case.date_of_birth = datetime.today().strftime("%y%m%d")
+            case.ehps.add(ehp1)
+            case.ehps.add(ehp2)
+            case.save()
+            case.digest = None
+
+        for i in range(10):
+            case = self.mk_case(gender='male', facility_code='333333')
+            case.date_of_birth = datetime.today().strftime("%y%m%d")
+            case.ehps.add(ehp1)
+            case.ehps.add(ehp2)
             case.save()
             case.digest = None
 
@@ -495,5 +521,6 @@ class DigestTest(MalariaTestCase):
         self.assertEqual(data['districts'][0]['males'], 0)
         self.assertEqual(data['districts'][0]['under5'], 10)
         self.assertEqual(data['districts'][0]['over5'], 0)
+        self.assertEqual(len(data['districts']), 2)
         self.assertEqual(
             message.to, ['manager@example.org', 'mis@example.org'])
