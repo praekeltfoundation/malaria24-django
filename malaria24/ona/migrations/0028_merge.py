53b92ef4b9c9677472732450f1f54c7c00367959
codiebeulaine <codiebeulaine@gmail.com>
2016-02-03 13:39:51 +0200

    adding migrations

---
+++ malaria24/ona/migrations/0028_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ona', '0027_auto_20160203_1049'),
+        ('ona', '0022_auto_20160202_1403'),
+    ]
+
+    operations = [
+    ]
