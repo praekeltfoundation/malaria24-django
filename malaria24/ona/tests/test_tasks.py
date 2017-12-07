2c95aae44d9c0322026f5e2e4524fd9299b84d8c
Milton Madanda <madandat@gmail.com>
2017-12-07 14:30:18 +0200

    add specific test for _other

--- malaria24/ona/tests/test_tasks.py
+++ malaria24/ona/tests/test_tasks.py
@@ -86,6 +86,34 @@ class OnaTest(MalariaTestCase):
         self.assertEqual(case.form, form)
 
     @responses.activate
+    def test_ona_fetch_reported_cases_task_data_capture_null_locality(self):
+        form = OnaForm.objects.create(uuid='uuuid', form_id='79925',
+                                      active=True)
+        self.assertEqual(len(ona_fetch_reported_cases()[form.form_id]), 2)
+        case = ReportedCase.objects.get(
+            _uuid='4c4799a9212245fcb564aa448444c3e0')
+        self.assertEqual(case.first_name, 'GHI')
+        self.assertEqual(case.last_name, 'KLM')
+        self.assertEqual(case.locality, '_other')
+        self.assertEqual(case.date_of_birth, '870910')
+        self.assertEqual(case.create_date_time.year, 2015)
+        self.assertEqual(case.create_date_time.month, 9)
+        self.assertEqual(case.create_date_time.day, 20)
+        self.assertEqual(case.sa_id_number, '0000000000000')
+        self.assertEqual(case.msisdn, '+27012345678')
+        self.assertEqual(case.id_type, 'No_SA_ID_Year_Entry')
+        self.assertEqual(case.abroad, '1')
+        self.assertEqual(case.reported_by, '+27123456789')
+        self.assertEqual(case.gender, 'male')
+        self.assertEqual(case.facility_code, '154342')
+        self.assertEqual(case.landmark, 'School')
+        self.assertEqual(case.landmark_description, 'Quite pretty')
+        self.assertEqual(case._id, '3608910')
+        self.assertEqual(case._uuid, '4c4799a9212245fcb564aa448444c3e0')
+        self.assertEqual(case._xform_id_string, 'reported_case')
+        self.assertEqual(case.form, form)
+
+    @responses.activate
     def test_compile_and_send_digest_email_noop(self):
         self.assertEqual(None, compile_and_send_digest_email())
         self.assertEqual([], mail.outbox)
