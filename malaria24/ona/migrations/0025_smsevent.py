1ac346b7617d58fa026fab40f2031a7b31d283eb
Kaitlyn Crawford <kaitlyn@praekelt.com>
2017-12-07 12:28:25 +0200

    Migrations

---
+++ malaria24/ona/migrations/0025_smsevent.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ona', '0024_inboundsms'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='SMSEvent',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('event_type', models.CharField(max_length=255)),
+                ('timestamp', models.DateTimeField()),
+                ('sms', models.ForeignKey(to='ona.SMS')),
+            ],
+        ),
+    ]
