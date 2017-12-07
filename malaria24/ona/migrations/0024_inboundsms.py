25702231075934803652ea25d6a5dd5b47b198bb
Kaitlyn Crawford <kaitlyn@praekelt.com>
2017-12-07 13:17:10 +0200

    Added the correct migration

---
+++ malaria24/ona/migrations/0024_inboundsms.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ona', '0023_actor_district'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='InboundSMS',
+            fields=[
+                ('message_id', models.UUIDField(serialize=False, primary_key=True)),
+                ('sender', models.CharField(max_length=255)),
+                ('content', models.TextField(blank=True)),
+                ('timestamp', models.DateTimeField(verbose_name=b'sent at')),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('reply_to', models.ForeignKey(to='ona.SMS', null=True)),
+            ],
+        ),
+    ]
