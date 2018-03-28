b66d5f66c4e257b4cb2e4d0a9706f1797304fe25
Onalerona Mosimege <msmona001@myuct.ac.za>
2018-03-28 10:33:37 +0200

    checked that the data has the number of objects expected

--- malaria24/ona/tests/test_models.py
+++ malaria24/ona/tests/test_models.py
@@ -497,31 +497,19 @@ class DigestTest(MalariaTestCase):
         self.assertEqual(message.to, [mis.email_address])
 
     @responses.activate
-    def test_print_district_digest_email_data(self):
+    def test_district_digest_email_data(self):
         Facility.objects.create(facility_code='342315',
                                 facility_name='Facility 1',
                                 province='Limpopo',
                                 district=u'Example1')
-        Facility.objects.create(facility_code='343434',
+        Facility.objects.create(facility_code='222222',
                                 facility_name='Facility 2',
                                 province='Limpopo',
-                                district=u'Example2')
-        Facility.objects.create(facility_code='333444',
+                                district=u'Example1')
+        Facility.objects.create(facility_code='333333',
                                 facility_name='Facility 3',
-                                province='Limpopo',
+                                province='The Eastern Cape',
                                 district=u'Example3')
-        Facility.objects.create(facility_code='333333',
-                                facility_name='Facility 3',
-                                province='The Eastern Cape',
-                                district=u'Example4')
-        Facility.objects.create(facility_code='111111',
-                                facility_name='Facility 1',
-                                province='Gauteng',
-                                district=u'Example5')
-        Facility.objects.create(facility_code='333333',
-                                facility_name='Facility 3',
-                                province='The Eastern Cape',
-                                district=u'Example5')
         manager1 = self.mk_actor(role=MANAGER_DISTRICT,
                                  email_address='manager@example.org',
                                  district=u'Example1', facility_code='342315')
@@ -561,52 +549,34 @@ class DigestTest(MalariaTestCase):
         digest = DistrictDigest.compile_digest()
         data = digest.get_digest_email_data(
             manager1.district, None)
-        # ADD PRINT DATA STATEMENT HERE TO VIEW DIGEST DATA
+        self.assertEqual(data['facility'][0]['females'], 10)
+        self.assertEqual(data['facility'][0]['males'], 0)
+        self.assertEqual(data['facility'][0]['facility'], 'Facility 1')
+        self.assertEqual(data['facility'][1]['facility'], 'Facility 2')
+        self.assertEqual(data['facility'][0]['under5'], 10)
+        self.assertEqual(data['facility'][0]['over5'], 0)
+        self.assertEqual(len(data['facility']), 2)
         self.assertEqual(
             data['week'], self.get_week(ReportedCase.objects.all()))
 
     @responses.activate
-    def test_print_national_digest_email_data(self):
+    def test_national_digest_email_data(self):
         Facility.objects.create(facility_code='342315',
                                 facility_name='Facility 1',
                                 province='Limpopo',
                                 district=u'Example1')
-        Facility.objects.create(facility_code='343333',
+        Facility.objects.create(facility_code='343434',
                                 facility_name='Facility 2',
                                 province='Limpopo',
                                 district=u'Example2')
-        Facility.objects.create(facility_code='345555',
-                                facility_name='Facility 1',
-                                province='Limpopo',
-                                district=u'Example6')
-        Facility.objects.create(facility_code='343333',
-                                facility_name='Facility 2',
-                                province='Limpopo',
-                                district=u'Example2')
-        Facility.objects.create(facility_code='333444',
-                                facility_name='Facility 3',
-                                province='Limpopo',
-                                district=u'Example3')
-        Facility.objects.create(facility_code='333441',
-                                facility_name='Facility 4',
-                                province='Limpopo',
-                                district=u'Example3')
-        Facility.objects.create(facility_code='333333',
-                                facility_name='Facility 3',
-                                province='The Eastern Cape',
-                                district=u'Example7')
-        Facility.objects.create(facility_code='111222',
-                                facility_name='Facility 2',
-                                province='Gauteng',
-                                district=u'Example5')
         Facility.objects.create(facility_code='111111',
                                 facility_name='Facility 1',
                                 province='Gauteng',
-                                district=u'Example5')
+                                district=u'Example4')
         Facility.objects.create(facility_code='333333',
                                 facility_name='Facility 3',
                                 province='The Eastern Cape',
-                                district=u'Example7')
+                                district=u'Example5')
         self.mk_actor(role=MANAGER_NATIONAL,
                       email_address='manager@example.org')
         ehp1 = self.mk_ehp(name='EHP1', email_address='ehp1@example.org')
@@ -622,7 +592,23 @@ class DigestTest(MalariaTestCase):
             case.digest = None
 
         for i in range(10):
-            case = self.mk_case(gender='male', facility_code='222222')
+            case = self.mk_case(gender='male', facility_code='333333')
+            case.date_of_birth = datetime.today().strftime("%y%m%d")
+            case.ehps.add(ehp1)
+            case.ehps.add(ehp2)
+            case.save()
+            case.digest = None
+
+        for i in range(10):
+            case = self.mk_case(gender='male', facility_code='111111')
+            case.date_of_birth = datetime.today().strftime("%y%m%d")
+            case.ehps.add(ehp1)
+            case.ehps.add(ehp2)
+            case.save()
+            case.digest = None
+
+        for i in range(10):
+            case = self.mk_case(gender='female', facility_code='343434')
             case.date_of_birth = datetime.today().strftime("%y%m%d")
             case.ehps.add(ehp1)
             case.ehps.add(ehp2)
@@ -631,7 +617,12 @@ class DigestTest(MalariaTestCase):
 
         digest = NationalDigest.compile_digest()
         data = digest.get_digest_email_data()
-        # ADD PRINT DATA STATEMENT HERE TO VIEW DIGEST DATA
+        self.assertEqual(len(data['provinces']), 4)
+        self.assertEqual(data['totals']['total_females'], 20)
+        self.assertEqual(data['totals']['total_males'], 20)
+        self.assertEqual(data['provinces'][1]['province'], 'Gauteng')
+        self.assertEqual(data['provinces'][1]['under5'], 10)
+        self.assertEqual(data['provinces'][1]['over5'], 0)
         self.assertEqual(
             data['week'], self.get_week(ReportedCase.objects.all()))
 
