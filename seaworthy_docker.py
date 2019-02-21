0b8cc239a6c16965536cfa0d3224b8b8e3642631
sewagodimo <edneciamat@gmail.com>
2019-02-21 13:22:42 +0200

    remove printline from tests

--- seaworthy_docker.py
+++ seaworthy_docker.py
@@ -12,5 +12,5 @@ fixture = container.pytest_fixture('malaria_container')
 
 def test_get(malaria_container):
 	response = malaria_container.http_client().get('/')
-	print(response)
 	assert response.status_code == 200
+	assert malaria_container.status() == 'running'
