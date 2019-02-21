1301435f9dd8667f6ba4cf1589ec76dd98cfa4e6
sewagodimo <edneciamat@gmail.com>
2019-02-21 10:59:32 +0200

    add seaworthy dockertests

---
+++ seaworthy_docker_test.py
@@ -0,0 +1,16 @@
+from seaworthy.definitions import ContainerDefinition
+
+container = ContainerDefinition(
+	name='test_malaria_container',
+	image='malaria',
+	wait_patterns=[r'Booting worker'],
+	create_kwargs={'ports': {'8000': None}},
+	wait_timeout=20)
+
+fixture = container.pytest_fixture('malaria_container')
+
+
+def test_get(malaria_container):
+	response = malaria_container.http_client().get('/')
+	print(response)
+	assert response.status_code == 200
