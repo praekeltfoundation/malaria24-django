38fd90f10a9429479600c1fecabef2bc79d89ff6
sewagodimo <edneciamat@gmail.com>
2019-02-22 10:58:38 +0200

    fix flake errors

--- malaria24/seaworthy/tests.py
+++ malaria24/seaworthy/tests.py
@@ -1,16 +1,16 @@
 from seaworthy.definitions import ContainerDefinition
 
 container = ContainerDefinition(
-	name='test_malaria_container',
-	image='malaria',
-	wait_patterns=[r'Booting worker'],
-	create_kwargs={'ports': {'8000': None}},
-	wait_timeout=20)
+    name='test_malaria_container',
+    image='malaria',
+    wait_patterns=[r'Booting worker'],
+    create_kwargs={'ports': {'8000': None}},
+    wait_timeout=20)
 
 fixture = container.pytest_fixture('malaria_container')
 
 
 def test_get(malaria_container):
-	response = malaria_container.http_client().get('/')
-	assert response.status_code == 200
-	assert malaria_container.status() == 'running'
+    response = malaria_container.http_client().get('/')
+    assert response.status_code == 200
+    assert malaria_container.status() == 'running'
