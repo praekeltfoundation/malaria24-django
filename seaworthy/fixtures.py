fd13bd29d92fd5265682570f5617ada4cb0f94ae
sewagodimo <edneciamat@gmail.com>
2019-02-25 15:43:14 +0200

    formatting fixes

--- seaworthy/fixtures.py
+++ seaworthy/fixtures.py
@@ -1,13 +1,11 @@
 from seaworthy.definitions import ContainerDefinition
 from seaworthy.containers.postgresql import PostgreSQLContainer
-from seaworthy.utils import output_lines
 
 
 class DjangoContainer(ContainerDefinition):
     WAIT_PATTERNS = (r"Booting worker",)
 
-    def __init__(self, name, db_url,
-                 image="malaria"):
+    def __init__(self, name, db_url, image="malaria"):
         super().__init__(name, image, self.WAIT_PATTERNS, wait_timeout=20)
         self.db_url = db_url
 
@@ -21,9 +19,6 @@ class DjangoContainer(ContainerDefinition):
                 "8000/tcp": ("127.0.0.1",),
             }
         }
-
-    def exec_django_admin(self, *args):
-        return output_lines(self.inner().exec_run(["django-admin"] + args))
 
 postgresql_container = PostgreSQLContainer("postgresql")
 postgresql_fixture, clean_postgresql_fixture = (
