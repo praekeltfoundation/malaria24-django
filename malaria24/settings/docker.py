28c9d05f856b245789b86998454cc4e343452486
sewagodimo <edneciamat@gmail.com>
2019-02-25 10:18:38 +0200

    remove docker databse settings

--- malaria24/settings/docker.py
+++ malaria24/settings/docker.py
@@ -5,6 +5,8 @@ from django.core.exceptions import ImproperlyConfigured
 from os import environ
 from os.path import abspath, dirname, join
 
+import dj_database_url
+
 from .production import *  # noqa: F401, F403
 
 
@@ -19,6 +21,10 @@ PROJECT_ROOT = (
     environ.get('PROJECT_ROOT') or dirname(dirname(abspath(__file__))))
 
 COMPRESS_OFFLINE = True
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///%s' % (join(PROJECT_ROOT, 'malaria24.sqlite3'),))}
 
 MEDIA_ROOT = join(PROJECT_ROOT, 'media')
 
