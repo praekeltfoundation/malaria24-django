ad637d0e55da8d76cb081aa99dbe6d4203069e84
sewagodimo <edneciamat@gmail.com>
2019-03-04 13:06:41 +0200

    set enviroment settings for the envvars in MC2

--- malaria24/settings/base.py
+++ malaria24/settings/base.py
@@ -41,7 +41,7 @@ ALLOWED_HOSTS = ['*']
 # Base URL to use when referring to full URLs within the Wagtail admin
 # backend - e.g. in notification emails. Don't include '/admin' or
 # a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = environ.get("BASE_URL") or 'http://example.com'
 
 
 # Application definition
@@ -122,9 +122,20 @@ CELERYBEAT_SCHEDULE = {
 DEFAULT_FROM_EMAIL = 'MalariaConnect <malaria24@praekelt.com>'
 
 # JEMBI settings
-JEMBI_URL = 'http://jembi.org/malaria24'
-JEMBI_USERNAME = 'fake@example.com'
-JEMBI_PASSWORD = 'not_a_real_password'
+JEMBI_URL = environ.get('JEMBI_URL') or 'http://jembi.org/malaria24'
+JEMBI_USERNAME = environ.get('JEMBI_USERNAME') or 'fake@example.com'
+JEMBI_PASSWORD = environ.get('JEMBI_PASSWORD') or 'not_a_real_password'
+
+ONAPIE_FORM_PK = environ.get('ONAPIE_FORM_PK') or ""
+RAVEN_DSN = environ.get('RAVEN_DSN') or ""
+ONAPIE_ACCESS_TOKEN = environ.get('ONAPIE_ACCESS_TOKEN') or ""
+
+# JUNEBUG Settings
+JUNEBUG_CHANNEL_URL = environ.get('JUNEBUG_CHANNEL_URL') or ""
+JUNEBUG_USERNAME = environ.get('JUNEBUG_USERNAME') or "junebug"
+JUNEBUG_PASSWORD = environ.get('JUNEBUG_PASSWORD') or "password"
+SMS_CHANNEL = environ.get('SMS_CHANNEL') or "junebug"
+SMS_CODE = environ.get('SMS_CODE') or "*31314"
 
 # Logging
 LOGGING = {
@@ -289,3 +300,4 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     )
 }
+
